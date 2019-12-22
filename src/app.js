0393328e0036ef03fda90502cf21ca2cb5adfbda
Quan Pham <doraemonfanclub@gmail.com>
2019-12-22 21:30:07 +0700

    Final code

--- src/app.js
+++ src/app.js
@@ -1,13 +1,38 @@
 const Discord = require('discord.js')
+const _ = require('lodash')
 const client = new Discord.Client()
+
+// https://discord.gg/jeBMuSW
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+const STREAMING_ROLE_ID = '652101620728856576'
 client.on('voiceStateUpdate', (oldState, newState) => {
-  console.log(newState.client.user)
-  console.log(newState.streaming)
+  const targetRole = newState.guild.roles.find(n => n.id === STREAMING_ROLE_ID)
+
+  if (newState.member.displayName !== 'Doko') return
+  if (!targetRole) return
+
+  if (newState.streaming) {
+    // Đang stream, set role.
+    newState.member.edit({
+      roles: _.concat(newState.member.roles.map(n => n.id), targetRole.id, n => n)
+    }).then((r) => {
+      // console.log(r)
+    }).catch(_ => {
+      // console.log(_)
+    })
+  } else {
+    newState.member.edit({
+      roles: newState.member.roles.filter(o => o.id !== STREAMING_ROLE_ID)
+    }).then((r) => {
+      // console.log(r)
+    }).catch(_ => {
+      // console.log(_)
+    })
+  }
 })
 
 client.login(process.env.DISCORD_TOKEN)
