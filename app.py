6415ded3774e3af24996f7e4aa56646e40f4b606
Quan Pham <doraemonfanclub@gmail.com>
2020-01-17 02:10:06 +0700

    Minor fix

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+import os
 import discord
+import settings
 
-class GVNStream(discord.Client)
+class GVNStream(discord.Client):
+  async def on_ready(self):
+    print('Logged in:', self.user)
+
+  async def on_voice_state_update(x, member, before, after):
+    print(member.display_name, after.self_stream)
+
+client = GVNStream()
+client.run(os.getenv("DISCORD_TOKEN"))
