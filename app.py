fb6dee458c3716042187c9156959c58958da4295
Quan Pham <doraemonfanclub@gmail.com>
2020-02-16 02:11:08 +0700

    Added server id

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ import os
 import discord
 import settings
 
-class GVNStream(discord.Client):
-  async def on_ready(self):
-    print('Logged in:', self.user)
+SERVER_ID = '479131273537912846'
+STREAM_ROLE_ID = '666132535650287626'
 
-  async def on_voice_state_update(x, member, before, after):
-    print(member.display_name, after.self_stream)
+active_streamers = []
 
-client = GVNStream()
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_voice_state_update(member, before, after):
+  print(member.display_name, after.self_stream)
+  stream_role_ref = discord.utils.get(member.guild.roles, id = STREAM_ROLE_ID)
+  print(stream_role_ref)
+
 client.run(os.getenv("DISCORD_TOKEN"))
